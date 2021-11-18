95591389114a791c603b2a83a48f1c6d867fc1e6
Waqar Khalid <waqarkhalid323@gmail.com>
2021-11-18 14:05:18 +0500

    second commit

---
+++ WaqarFramework.xcframework/ios-arm64_x86_64-simulator/WaqarFramework.framework/Headers/WaqarFramework.h
@@ -0,0 +1,18 @@
+//
+//  WaqarFramework.h
+//  WaqarFramework
+//
+//  Created by Waqar Khalid on 11/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaqarFramework.
+FOUNDATION_EXPORT double WaqarFrameworkVersionNumber;
+
+//! Project version string for WaqarFramework.
+FOUNDATION_EXPORT const unsigned char WaqarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaqarFramework/PublicHeader.h>
+
+
---
+++ WaqarFramework/FruitGenerator.swift
@@ -0,0 +1,14 @@
+//
+//  FruitGenerator.swift
+//  WaqarFramework
+//
+//  Created by Waqar Khalid on 11/18/21.
+//
+
+import Foundation
+public class FruitGenerator{
+    public init(){}
+    public func getFruitName() -> String{
+        return "Here is your fruit name boy, Orange"
+    }
+}
